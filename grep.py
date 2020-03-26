aa39c1cad384b7f07bee5f6eb67c186da10ea5f3
Veronika Hlavackova <veronikahlavackova@jupyter.korpus.cz>
2020-03-26 17:10:53 +0100

    Ošetření chyb

--- grep.py
+++ grep.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+# dosctring = dokumentační řetězec; uloží se do 
+#kouzelné proměnné _doc_
+"""Usage: grep.py PATTERN FILE
 
+Print lines from FILE matching regular expression PATTERN.
+
+"""
 import sys
 import regex as re
 
-pattern, path = sys.argv[1:]
-with open(path) as file:
-    for line in file:
-        line = line.strip("\n")
-        if re.search(pattern, line):
-            print(line)
+try: 
+    pattern, path = sys.argv[1:]
+except ValueError: 
+    print(__doc__.strip(), file=sys.stderr)
+    sys.exit(1)
+
+try:    
+    with open(path) as file:
+        for line in file:
+            line = line.strip("\n")
+            if re.search(pattern, line):
+                print(line)
+except FileNotFoundError as err:
+    print(__doc__.strip(), file=sys.stderr)
+    print(err, file=sys.stderr)
+    sys.exit(1)
