ad45bd51076003406a425be8b54ddb2777a1c8ef
Veronika Hlavackova <veronikahlavackova@jupyter.korpus.cz>
2020-03-26 16:46:48 +0100

    Regulární výrazy

--- grep.py
+++ grep.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
+import regex as re
 
 pattern, path = sys.argv[1:]
 with open(path) as file:
     for line in file:
-        if pattern in line:
-            print(line, end='')
+        line = line.strip("\n")
+        if re.search(pattern, line):
+            print(line)
