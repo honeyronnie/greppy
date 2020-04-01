958ed7c6dcaf52d22800875c0f77215e76c02a47
Veronika Hlavackova <veronikahlavackova@jupyter.korpus.cz>
2020-04-01 16:56:16 +0200

    Prohledat víc souborů najednou.

--- grep.py
+++ grep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Usage: grep.py PATTERN FILE
+"""Usage: grep.py PATTERN FILE [FILE...]
 
-Print lines from FILE matching regular expression PATTERN.
+Print lines from each FILE matching regular expression PATTERN.
 
 """
 
@@ -12,30 +12,35 @@ import regex as re
 def grep(pattern, lines):
     """Print lines matching pattern."""
     for line in lines:
-        line=line.strip()
+        line = line.strip()
         if re.search(pattern, line):
             print(line)
 
             
 def parse_argv(argv):
-    pattern, path =argv[1:]
-    return pattern, path
+    """Parse script arguments."""
+    if len(argv) < 2:
+        raise ValueError
+    pattern, paths = argv[0], argv[1:]
+    return pattern, paths
 
 
-def main():
+def main(argv):
+    """Main entry point of script."""
     try: 
-        pattern, path = parse_argv(sys.argv)
+        pattern, paths = parse_argv(argv)
     except ValueError: 
         print(__doc__.strip(), file=sys.stderr)
         sys.exit(1)
+    for path in paths:
+        try:    
+            with open(path) as file:
+                grep(pattern, file)
+        except FileNotFoundError as err:
+            print(__doc__.strip(), file=sys.stderr)
+            print(err, file=sys.stderr)
+            sys.exit(1)
 
-    try:    
-        with open(path) as file:
-            grep(pattern, file)
-    except FileNotFoundError as err:
-        print(__doc__.strip(), file=sys.stderr)
-        print(err, file=sys.stderr)
-        sys.exit(1)
         
 if __name__ == "__main__":
-        main()
+    main(sys.argv[1:])
